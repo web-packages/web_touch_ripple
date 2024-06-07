635814874ff3f633fff060a6bfb5009b3493449d
MTtankkeo <ttankkeo112@gmail.com>
2024-06-07 13:53:44 +0900

    temp update

---
+++ jsx/src/index.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useLayoutEffect, useRef } from "react";
+
+// @ts-ignore
+import { TouchRippleElement } from "web-touch-ripple";
+
+export function TouchRipple({onTap, onDoubleTap, wait, children}: {
+    onTap?: VoidFunction,
+    onDoubleTap?: VoidFunction,
+    wait?: boolean,
+    children: ReactNode,
+}) {
+    const ref = useRef<TouchRippleElement>();
+
+    useLayoutEffect(() => {
+        const ripple = ref.current;
+        ripple.ontap = onTap;
+        ripple.ondoubletap = onDoubleTap;
+        
+        wait ? ripple.setAttribute("wait", "") : ripple.removeAttribute("wait");
+    }, [onTap, onDoubleTap, wait]);
+
+    return (
+        /** @ts-ignore */
+        <touch-ripple ref={ref}>{children}</touch-ripple>
+    );
+}
